cb0c9652ea18315747c4393af3654dffd992f1b4
AqeelArshad811 <aqeelarshad811@gmail.com>
2025-04-16 12:19:22 +0500

    update image import in links

--- FashionShop/src/components/Links.js
+++ FashionShop/src/components/Links.js
@@ -1,55 +1,55 @@
 import hero1 from "../assets/hero1.webp";
 import hero2 from "../assets/hero2.webp";
 import hero3 from "../assets/hero3.jpg";
-import support24Hours from "../assets/featureImg/support24Hours.png";
+import support24Hours from "../assets/featureImg/support24hours.png";
 import freeShipping from "../assets/featureImg/freeShipping.png";
-import securePayment from "../assets/featureImg/securePayment.png";
+import tryVirtually from "../assets/featureImg/tryVirtually.png";
 import easyReturn from "../assets/featureImg/easyReturn.png";
-import tryVirtually from "../assets/featureImg/TryVirtually.png";
+
 // import return 
 // import { FaShippingFast } from "react-icons/fa";
 
 export const Links = [
-    {
-      id:1,
-      navLink:"Home",
-      toLink:"/"
-    },
-    {
-      id:2,
-      navLink:"Products",
-      toLink:"/products"
-    },
-    {
-      id:3,
-      navLink:"About",
-      toLink:"/about"
-    },
-    {
-      id:4,
-      navLink:"Checkout",
-      toLink:"/checkout"
-    },
-    {
-      id:5,
-      navLink:"Cart",
-      toLink:"/cart"
-    },
-    
-  ]
+  {
+    id: 1,
+    navLink: "Home",
+    toLink: "/"
+  },
+  {
+    id: 2,
+    navLink: "Products",
+    toLink: "/products"
+  },
+  {
+    id: 3,
+    navLink: "About",
+    toLink: "/about"
+  },
+  {
+    id: 4,
+    navLink: "Checkout",
+    toLink: "/checkout"
+  },
+  {
+    id: 5,
+    navLink: "Cart",
+    toLink: "/cart"
+  },
+
+]
 
 export const HeroData = [
   {
-    id:1,
-    img:hero1,
-    title:"The Fashion You Like",
-    desc:"Try what you like, Ask what you want"
+    id: 1,
+    img: hero1,
+    title: "The Fashion You Like",
+    desc: "Try what you like, Ask what you want"
   },
   {
-    id:2,
-    img:hero2,
-    title:"Try Room",
-    desc:"just upload your image and try the dress"
+    id: 2,
+    img: hero2,
+    title: "Try Room",
+    desc: "just upload your image and try the dress"
   },
   // {
   //   id:3,
@@ -68,27 +68,27 @@ export const HeroData = [
 
 export const FeaturesData = [
   {
-    id:0,
-    img:tryVirtually,
-    title:"Try Room",
-    desc:"Try the dress virtually "
+    id: 0,
+    img: tryVirtually,
+    title: "Try Room",
+    desc: "Try the dress virtually "
   },
   {
-    id:2,
-    img:support24Hours,
-    title:"Customer Support",
-    desc:"24/7 customer support available"
+    id: 2,
+    img: support24Hours,
+    title: "Customer Support",
+    desc: "24/7 customer support available"
   },
   {
-    id:1,
-    img:freeShipping,
-    title:"Free Shipping",
-    desc:"Get free shipping on all orders over 5000 Rs"
+    id: 1,
+    img: freeShipping,
+    title: "Free Shipping",
+    desc: "Get free shipping on all orders over 5000 Rs"
   },
   {
-    id:4,
-    img:easyReturn,
-    title:"Easy Return",
-    desc:"Easy return policy available"
+    id: 4,
+    img: easyReturn,
+    title: "Easy Return",
+    desc: "Easy return policy available"
   }
 ]
